906ac4bdaf1ecd82e466ed8ffa44325739251ec4
IvanKalabanan <ivanito9@mail.ru>
2015-04-24 09:22:02 +0300

    Add Gson

--- app/src/main/java/com/stfalcon/whoisthere/FacebookActivity.java
+++ app/src/main/java/com/stfalcon/whoisthere/FacebookActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.JsonReader;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -20,17 +21,24 @@ import com.facebook.login.widget.LoginButton;
 import com.facebook.FacebookException;
 import com.facebook.login.widget.ProfilePictureView;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONObject;
 import org.json.JSONException;
 
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 
 
 public class FacebookActivity extends ActionBarActivity {
 
-    LoginButton loginButton; TextView nameT,idT,linkT;
+    LoginButton loginButton; TextView TextView_Name,TextView_Id,TextView_Link;
     ProfilePictureView profilePictureView;
 
     CallbackManager callbackManager;
@@ -42,62 +50,88 @@ public class FacebookActivity extends ActionBarActivity {
         callbackManager.onActivityResult(requestCode, responseCode, data);
     }
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        InitializationFacebook(savedInstanceState);
+
+        InitializationView();
+
+        CheckUserLogin();
+
+        LoginButtonCLick();
+
+       /* List<String> permissionNeeds = Arrays.asList("user_photos", "email", "user_birthday", "user_location", "public_profile");
+
+        loginButton.setReadPermissions(permissionNeeds);*/
+
+    }
+
+
+    public void InitializationFacebook(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(getApplicationContext());
         callbackManager = CallbackManager.Factory.create();
 
         setContentView(R.layout.activity_facebook);
+    }
 
+    public void InitializationView()
+    {
         loginButton = (LoginButton)this.findViewById(R.id.login_button);
-        nameT= (TextView)this.findViewById(R.id.textView);
-        linkT= (TextView)this.findViewById(R.id.textViewEmail);
-        idT= (TextView)this.findViewById(R.id.textViewId);
+        TextView_Name= (TextView)this.findViewById(R.id.textView);
+        TextView_Link= (TextView)this.findViewById(R.id.textViewEmail);
+        TextView_Id= (TextView)this.findViewById(R.id.textViewId);
+    }
 
+    public void CheckUserLogin()
+    {
         if(AccessToken.getCurrentAccessToken()!=null)
         {
-            nameT.setText(Profile.getCurrentProfile().getName());
-            idT.setText(Profile.getCurrentProfile().getId());
+            TextView_Name.setText(Profile.getCurrentProfile().getName());
+            TextView_Id.setText(Profile.getCurrentProfile().getId());
             profilePictureView = (ProfilePictureView) findViewById(R.id.friendProfilePicture);
             profilePictureView.setProfileId(Profile.getCurrentProfile().getId());
-            linkT.setText(Profile.getCurrentProfile().getLinkUri().toString());
+            TextView_Link.setText(Profile.getCurrentProfile().getLinkUri().toString());
         } else
         {
             Toast toast = Toast.makeText(getApplicationContext(),
                     "You must Login", Toast.LENGTH_SHORT);
             toast.show();
         }
-       /* List<String> permissionNeeds = Arrays.asList("user_photos", "email", "user_birthday", "user_location", "public_profile");
+    }
 
-        loginButton.setReadPermissions(permissionNeeds);*/
-
+    public void LoginButtonCLick()
+    {
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
 
             @Override
             public void onSuccess(LoginResult loginResult) {
-                GraphRequest request = GraphRequest.newMeRequest
+
+
+
+                final GraphRequest request = GraphRequest.newMeRequest
                         (loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
                             @Override
                             public void onCompleted(JSONObject object, GraphResponse response) {
                                 try {
-                                    String id = object.getString("id");
-                                    String name = object.getString("name");
-                                    String link = object.getString("link");
 
-                                    User user = new User(name,id,link);
+                        User user =  new Gson().fromJson(object.toString(), User.class);
 
-                                    nameT.setText(user.getName());
-                                    idT.setText(user.getId());
 
-                                    profilePictureView = (ProfilePictureView) findViewById(R.id.friendProfilePicture);
-                                    profilePictureView.setProfileId(user.getId());
+                                        TextView_Name.setText(user.name);
+                                        TextView_Id.setText(user.id);
 
-                                    //String url = "http://graph.facebook.com/839669416127599/picture";
+                                        profilePictureView = (ProfilePictureView) findViewById(R.id.friendProfilePicture);
+                                        profilePictureView.setProfileId(user.id);
 
-                                    linkT.setText(user.getLink());
+                                        //String url = "http://graph.facebook.com/839669416127599/picture";
 
-                                } catch (JSONException e) {
+                                        TextView_Link.setText(user.link);
+
+                                } catch (Exception e) {
                                     e.printStackTrace();
                                 }
 
@@ -124,32 +158,6 @@ public class FacebookActivity extends ActionBarActivity {
                 Log.v("LoginActivity", exception.getCause().toString());
             }
         });
-
     }
-
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_facebook, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
 
 }
